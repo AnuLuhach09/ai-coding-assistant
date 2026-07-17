b850239dd6c05ce274623b928baa8526f2846d0f
AnuLuhach09 <anuluhach09@gmail.com>
2026-07-18 01:42:41 +0530

    Fix CORS syntax

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -60,7 +60,7 @@ app.use(cors({
 }));
 
 app.options("*", cors());
-);
+
 app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
